85232e732659ec430edb8d73434558dd869976f1
Jolanda Besteman <jebesteman@gmail.com>
2021-11-05 12:46:12 +0100

    added changes container

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Container from "./Container";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="groceries-header">
+        <h1>Jolanda's boodschappenlijst</h1>
       </header>
+      <Container />
     </div>
   );
 }
